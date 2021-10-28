cf96e11ae5fd30b54f09d045506a90289fb80082
Reign <reignzj.westry@gmail.com>
2021-10-28 01:55:57 -0400

    ADDED dice roll functionality

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/*
+  Objective:
+    PIG Game
+
+ */
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+
+
+// Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function(){
+  // 1. Generate a random dice roll
+  const dice = Math.trunc(Math.random()* 6) +1;
+  // 2. Display dice
+      diceEl.classList.remove('hidden');
+      diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check if rolled dice === 1: if true, switch to new player
+        if(dice !== 1 ){
+          // Add dice to current score
+          // currentScore = currentScore + dice;
+          //note Shorthand
+          currentScore += dice;
+        } else {
+          // Switch to next player
+
+        }
+
+})
+btnNew.addEventListener('click', function(){
+
+})
+btnHold.addEventListener('click', function(){
+
+})
